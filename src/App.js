c6710a092bc4160529c07c2c7019d4d376958266
Joseph Akpu <63398509+akpu-jo@users.noreply.github.com>
2022-10-30 14:01:05 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Users from './pages/Users';
+import About from './pages/About';
+import NoMatch from './pages/404';
+import AppBar from './components/AppBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <AppBar />
+      <Routes>
+        <Route path={`/`} element={<Home />} />
+        <Route path={`/users`} element={<Users />} />
+        <Route path={`/about`} element={<About />} />
+        <Route path={`*`} element={<NoMatch />} />
+        {/* <Route path={`/users`} element={<Users />} /> */}
+       
+      </Routes>
+    </BrowserRouter>
   );
 }
 
